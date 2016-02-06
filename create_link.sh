3be000d773928781180c64f2eb14c9f7148fd958
cyen <CYen@walmartlabs.com>
2016-02-05 16:01:10 -0800

    .

--- create_link.sh
+++ create_link.sh
@@ -4,3 +4,4 @@ ln -s ~/configs/tmux.conf ~/.tmux.conf
 ln -s ~/configs/bashrc ~/.bashrc
 ln -s ~/configs/gitconfig ~/.gitconfig
 ln -s ~/configs/vim ~/.vim
+ln -s ~/configs/mysql_prod.sh ~/mysql_prod.sh
