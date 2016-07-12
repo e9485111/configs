2cce389accb58a52c9f82d119eaeb1c5c2c097bc
cyen <CYen@walmartlabs.com>
2016-07-11 17:07:55 -0700

    .

--- create_link.sh
+++ create_link.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 install_fasd=0
+rm ~/.vimrc
 ln -s ~/configs/vimrc ~/.vimrc
+rm ~/.tmux.conf
 ln -s ~/configs/tmux.conf ~/.tmux.conf
+rm ~/.bashrc
 ln -s ~/configs/bashrc ~/.bashrc
+rm ~/.gitconfig
 ln -s ~/configs/gitconfig ~/.gitconfig
+rm ~/.vim
 ln -s ~/configs/vim ~/.vim
 #ln -s ~/configs/mysql_prod.sh ~/mysql_prod.sh
 #ln -s ~/configs/mysql_dev.sh ~/mysql_dev.sh
+rm ~/.ssh/config
 ln -s ~/configs/config ~/.ssh/config
+rm ~/.slate
 ln -s ~/configs/slate ~/.slate
 chmod go-w ~/.ssh/config
 if [ $install_fasd -eq 1 ];
