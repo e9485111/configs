b0e6677a5104917e98ebb664b443c5210903ad85
cyen <CYen@walmartlabs.com>
2016-08-11 11:22:19 -0700

    .

--- copy_ssh_config.sh
+++ copy_ssh_config.sh
@@ -1,5 +1,13 @@
-for i in `cat hosts`;
-do
-  echo $i
-  scp ~/configs/config $i:./configs
-done
+if [ "$1" ];
+then
+  echo $1
+  scp ~/.ssh/* $i:./.ssh
+  scp ~/configs/config $1:./configs
+else
+  for i in `cat hosts`;
+  do
+    echo $i
+    scp ~/.ssh/* $i:./.ssh
+    scp ~/configs/config $i:./configs
+  done
+fi
