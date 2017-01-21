62437a07927ac5b0947abb1173ec8a3c10294e69
cyen <cyen@godaddy.com>
2017-01-21 00:47:37 +0000

    .

--- copy_ssh_config.sh
+++ copy_ssh_config.sh
@@ -1,13 +1,12 @@
+tar zcf ~/ssh.tar.gz ~/.ssh
+tar zcf ~/configs.tar.gz ~/configs
 if [ "$1" ];
 then
   echo $1
-  scp ~/.ssh/* $i:./.ssh
-  scp ~/configs/config $1:./configs
+  scp ~/ssh.tar.gz ~/configs.tar.gz $1:.
+  ssh $1 "rm -rf configs.bak .ssh.bak; mv .ssh .ssh.bak; mv configs configs.bak; tar zxf ssh.tar.gz -C /; tar zxf configs.tar.gz -C /; rm -rf ~/.ssh.bak ~/.configs.bak ~/ssh.tar.gz ~/configs.tar.gz"
 else
-  for i in `cat hosts`;
-  do
-    echo $i
-    scp ~/.ssh/* $i:./.ssh
-    scp ~/configs/config $i:./configs
-  done
+  pscp -h hosts ~/ssh.tar.gz ~/configs.tar.gz ~/
+  pssh -h hosts "rm -rf configs.bak .ssh.bak; mv .ssh .ssh.bak; mv configs configs.bak; tar zxf ssh.tar.gz -C /; tar zxf configs.tar.gz -C /; rm -rf ~/.ssh.bak ~/.configs.bak ~/ssh.tar.gz ~/configs.tar.gz"
 fi
+rm ~/ssh.tar.gz ~/configs.tar.gz
