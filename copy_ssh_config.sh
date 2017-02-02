2c08453cc43fa90c1e0b886b138b8632e43b8dd2
cyen <cyen@godaddy.com>
2017-02-02 06:14:55 +0000

    .

--- copy_ssh_config.sh
+++ copy_ssh_config.sh
@@ -3,10 +3,10 @@ tar zcf ~/configs.tar.gz ~/configs
 if [ "$1" ];
 then
   echo $1
-  scp ~/ssh.tar.gz ~/configs.tar.gz $1:.
-  ssh $1 "rm -rf configs.bak .ssh.bak; mv .ssh .ssh.bak; mv configs configs.bak; tar zxf ssh.tar.gz -C /; tar zxf configs.tar.gz -C /; rm -rf ~/.ssh.bak ~/.configs.bak ~/ssh.tar.gz ~/configs.tar.gz"
+  scp -o "StrictHostKeyChecking no" ~/ssh.tar.gz ~/configs.tar.gz $1:.
+  ssh -o "StrictHostKeyChecking no" $1 "rm -rf configs.bak .ssh.bak; mv .ssh .ssh.bak; mv configs configs.bak; tar zxf ssh.tar.gz -C /; tar zxf configs.tar.gz -C /; rm -rf ~/.ssh.bak ~/.configs.bak ~/ssh.tar.gz ~/configs.tar.gz"
 else
-  pscp -h hosts ~/ssh.tar.gz ~/configs.tar.gz ~/
-  pssh -h hosts "rm -rf configs.bak .ssh.bak; mv .ssh .ssh.bak; mv configs configs.bak; tar zxf ssh.tar.gz -C /; tar zxf configs.tar.gz -C /; rm -rf ~/.ssh.bak ~/.configs.bak ~/ssh.tar.gz ~/configs.tar.gz"
+  pscp -o "StrictHostKeyChecking no" -h hosts ~/ssh.tar.gz ~/configs.tar.gz ~/
+  pssh -o "StrictHostKeyChecking no" -h hosts "rm -rf configs.bak .ssh.bak; mv .ssh .ssh.bak; mv configs configs.bak; tar zxf ssh.tar.gz -C /; tar zxf configs.tar.gz -C /; rm -rf ~/.ssh.bak ~/.configs.bak ~/ssh.tar.gz ~/configs.tar.gz"
 fi
 rm ~/ssh.tar.gz ~/configs.tar.gz
